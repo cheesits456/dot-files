5cf948dac81641afa821519dfdb38d1023a6e66c
cheesits456 <quin@cheesits456.dev>
2020-10-03 16:51:29 -0700

    Create ls-colors.sh

---
+++ bashrc-additions/ls-colors.sh
@@ -0,0 +1,12 @@
+## Add ls-colors command to print $LS_COLORS in a nicer format
+## Bugs:
+## - column command processes color escape sequences as printable characters
+ls-colors() {
+	echo -e $(
+		printf "%s" $LS_COLORS |
+			sed 's/:/\n/g' |
+			awk -F\= '{print "\\e[" $2 "m" $0 "\\e[0m"}'
+		) |
+		sed 's/ /\n/g' |
+		column
+}
