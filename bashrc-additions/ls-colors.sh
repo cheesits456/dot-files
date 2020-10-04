5ed0f299c994dd122e1ed07e2252b97623ea4ad1
cheesits456 <quin@cheesits456.dev>
2020-10-03 18:30:22 -0700

    Update ls-colors.sh

--- bashrc-additions/ls-colors.sh
+++ bashrc-additions/ls-colors.sh
@@ -1,6 +1,7 @@
 ## Add ls-colors command to print $LS_COLORS in a nicer format
 ## Bugs:
-## - column command processes color escape sequences as printable characters
+## - column command processes color escape sequences as printable characters,
+##     which can effect column alignment
 ls-colors() {
 	echo -e $(
 		printf "%s" $LS_COLORS |
@@ -8,5 +9,6 @@ ls-colors() {
 			awk -F\= '{print "\\e[" $2 "m" $0 "\\e[0m"}'
 		) |
 		sed 's/ /\n/g' |
-		column
+		column |
+		column -t
 }
