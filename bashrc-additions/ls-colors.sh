8ebc53e110fcdbf9081688b9819f96f59aebfb41
cheesits456 <quin@cheesits456.dev>
2020-10-03 21:49:35 -0700

    Update ls-colors.sh

--- bashrc-additions/ls-colors.sh
+++ bashrc-additions/ls-colors.sh
@@ -3,12 +3,24 @@
 ## - column command processes color escape sequences as printable characters,
 ##     which can effect column alignment
 ls-colors() {
-	echo -e $(
-		printf "%s" $LS_COLORS |
+	command -v node > /dev/null && (
+		echo -e $(
+			js="let ls=process.env.LS_COLORS"
+			js+="	.split(':')"
+			js+="	.filter(a => a)"
+			js+="	.sort((a, b) => a.localeCompare(b));"
+			js+="console.log("
+			js+="	ls"
+			js+="		.filter(a => a.match(/^\w/))"
+			js+="		.concat(ls.filter(a => a.match(/^\W/)))"
+			js+="		.join('\n')"
+			js+=")"
+			node -e "$js" |
 			sed 's/:/\n/g' |
-			awk -F\= '{print "\\e[" $2 "m" $0 "\\e[0m"}'
+			awk -F\= '{print "\\e[" $2 "m" $1 "\\t" $2 "\\e[0m"}'
 		) |
 		sed 's/ /\n/g' |
-		column |
+		column -c 100 |
 		column -t
+	) || echo "You must install NodeJS to use this function"
 }
